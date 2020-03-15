8f2b44b1c89c4cc8dd2137645390b844d382ad19
ductu <duc.tu.le.anh@gmail.com>
2020-03-15 14:31:31 +0100

    Simplify UserPrivilegeInputDataProvider

--- src/main/java/com/userprivilegescheduler/io/UserPrivilegeInputDataProvider.java
+++ src/main/java/com/userprivilegescheduler/io/UserPrivilegeInputDataProvider.java
@@ -4,38 +4,50 @@ import com.userprivilegescheduler.models.UserPrivilege;
 import com.userprivilegescheduler.models.UserPrivilegeInput;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
+/**
+ * Provides meta data which is not explicitly given in the {@link UserPrivilegeInput}. E.g number of users and
+ * number of privileges.
+ */
 public class UserPrivilegeInputDataProvider {
 
     public static final String USER_KEY = "users";
     public static final String PRIVILEGE_KEY = "privileges";
 
     /**
-     * Reads the {@link UserPrivilegeInput} to determine the numbers of users and
-     * numbers of privileges. The result will be returned in a map.
+     * Reads the {@link UserPrivilegeInput} to determine the number of users and
+     * number of privileges. The result will be returned in a map.
+     *
      * @param userInput the user input to read
-     * @return numbers of users and privileges in a map
+     * @return number of users and privileges in a map
      */
-    public static Map<String, Integer> getEntityNumbers(UserPrivilegeInput userInput){
-        Set<Integer> setOfUsers = new HashSet<>(),  setOfPrivileges = new HashSet<>();
-
-        fillEntitySets(setOfUsers, setOfPrivileges, userInput.getUserPrivileges());
-        fillEntitySets(setOfUsers, setOfPrivileges, userInput.getForbiddenUserPrivileges());
-
-        return createMapWithEntityNumbers(setOfUsers, setOfPrivileges);
-    }
-
-    private static HashMap<String, Integer> createMapWithEntityNumbers(Set<Integer> setOfUsers, Set<Integer> setOfPrivileges) {
-        HashMap<String, Integer> entityNumbers = new HashMap<String, Integer>();
-        entityNumbers.put(USER_KEY, setOfUsers.size());
-        entityNumbers.put(PRIVILEGE_KEY, setOfPrivileges.size());
+    public static Map<String, Integer> getEntityNumbers(UserPrivilegeInput userInput) {
+        HashMap<String, Integer> entityNumbers = new HashMap<>();
+        entityNumbers.put(USER_KEY, getAllUsers(userInput).size());
+        entityNumbers.put(PRIVILEGE_KEY, getAllPrivileges(userInput).size());
         return entityNumbers;
     }
 
-    private static void fillEntitySets(Set<Integer> setOfUsers, Set<Integer> setOfPrivileges, List<UserPrivilege> input){
-        input.forEach(userPrivilege -> {
-            setOfUsers.add(userPrivilege.getUser());
-            setOfPrivileges.add(userPrivilege.getPrivilege());
-        });
+    private static Set<Integer> getAllPrivileges(UserPrivilegeInput userInput) {
+        Set<Integer> allPrivilegesSet = userInput.getUserPrivileges().stream()
+                .map(UserPrivilege::getPrivilege)
+                .collect(Collectors.toSet());
+        allPrivilegesSet.addAll(
+                userInput.getForbiddenUserPrivileges().stream()
+                        .map(UserPrivilege::getPrivilege)
+                        .collect(Collectors.toSet()));
+        return allPrivilegesSet;
+    }
+
+    private static Set<Integer> getAllUsers(UserPrivilegeInput userInput) {
+        Set<Integer> allUsersSet = userInput.getUserPrivileges().stream()
+                .map(UserPrivilege::getUser)
+                .collect(Collectors.toSet());
+        allUsersSet.addAll(
+                userInput.getForbiddenUserPrivileges().stream()
+                        .map(UserPrivilege::getUser)
+                        .collect(Collectors.toSet()));
+        return allUsersSet;
     }
 }
